0bd612b5d6489addc3d6343034e7eaa02ddc0e3e
Matthew Golden <mgolden91@gmail.com>
2016-01-02 15:29:58 -0600

    Checkpoint 20

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -11,10 +11,11 @@ class MenuController
 
     puts "Main Menu - #{@address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View Entry Number n"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection:"
 
     selection = gets.to_i
@@ -27,20 +28,25 @@ class MenuController
 
     when 2
       system "clear"
-      create_entry
+      entry_n_submenu
       main_menu
 
     when 3
       system "clear"
-      search_entries
+      create_entry
       main_menu
 
     when 4
       system "clear"
-      read_csv
+      search_entries
       main_menu
 
     when 5
+      system "clear"
+      read_csv
+      main_menu
+
+    when 6
       puts "Good-bye"
 
       exit(0)
@@ -63,6 +69,21 @@ class MenuController
 
     system "clear"
     puts "End of entries"
+  end
+
+  def entry_n_submenu
+    print "Entry number to view: "
+    selection = gets.chomp.to_i
+
+    if selection < @address_book.entries.count
+      puts @address_book.entries[selection]
+      puts "Press enter to return to the main menu"
+      gets.chomp
+      system "clear"
+    else
+      puts "#{selection} is not a valid input"
+      entry_n_submenu
+    end
   end
 
   def create_entry
